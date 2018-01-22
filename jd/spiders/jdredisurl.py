782c54d74f3f739fc4b15d926ac8ea6cdd5f051a
renqian520 <35051100+renqian520@users.noreply.github.com>
2018-01-22 18:56:26 +0800

    Update jdredisurl.py

--- jd/spiders/jdredisurl.py
+++ jd/spiders/jdredisurl.py
@@ -15,10 +15,10 @@ class Ecspider(scrapy.Spider):
     }
     start_urls = []
     for keywords in open('D:\\Scrapy\\jd\\jd\\keywords.txt', encoding='utf-8'):
-        keyword = urllib.request.quote(keywords).replace('\n','')
+        keyword = urllib.request.quote(keywords).replace('\\n','')
         #奇数页
         for i in range(1,10,2):
-            start_urls.append('https://search.jd.com/Search?keyword={}&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&cid2=653&cid3=655&page={}&s=1&click=0'.format(str(keyword),str(i)))
+            start_urls.append('https://search.jd.com/Search?keyword={}&enc=utf-8&qrst=1&rt=1&stop=1&vt=2&page={}&s=1&click=0'.format(str(keyword),str(i)))
 
 
 
@@ -26,4 +26,5 @@ class Ecspider(scrapy.Spider):
         item = RedisItem()
         item['url'] = response.url
         yield item
+
 
